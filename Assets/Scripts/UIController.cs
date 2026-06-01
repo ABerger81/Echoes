5b3d615fdf81b133de923118a51b5d76970f2d46
Andreas Berger <aberger1981@outlook.com>
2026-06-01 14:45:24 +0200

    Add new materials, textures, and UI controller for interaction prompts
    - Created a new material for the OldDoorLeft.
    - Updated the ScaryHorror scene with new GameObjects including cubes and walls with appropriate components.
    - Implemented a UIController script to manage interaction prompts and visibility of UI elements.
    - Added textures for OldDoorLeft and OldDoorRight, including normal maps.
    - Updated metadata for all new assets to ensure proper import settings.

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script is responsible for controlling the UI elements that appear when the player can interact with objects in the game world. It updates the action text and command text based on the player's current context and manages the visibility of the UI elements.
+public class UIController : MonoBehaviour
+{
+    public static string actionText;
+    public static string commandText;
+    public static bool uiActive;
+    [SerializeField] GameObject actionBox;
+    [SerializeField] GameObject commandBox;
+    [SerializeField] GameObject interactCross;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (uiActive == true)
+        {
+            actionBox.SetActive(true);
+            commandBox.SetActive(true);
+            interactCross.SetActive(true);
+            actionBox.GetComponent<TMPro.TMP_Text>().text = actionText;
+            commandBox.GetComponent<TMPro.TMP_Text>().text = "[E] " + commandText;
+        }
+        else
+        {
+            actionBox.SetActive(false);
+            commandBox.SetActive(false);
+            interactCross.SetActive(false);
+        }
+    }
+}
